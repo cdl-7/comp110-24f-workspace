1175dea72480ceca29bbb6fd855255f976da298b
Cooper Lair <cdlair@unc.edu>
2024-09-12 13:23:16 -0400

    Finished EX02

---
+++ exercises/ex02_chardle.py
@@ -0,0 +1,55 @@
+"""Exercise 02 Chardle"""
+
+__author__ = "730742183"
+
+
+def input_word() -> str:
+    word: str = input("Enter a 5-character word: ")
+    if len(word) == 5:
+        return word
+    else:
+        print("Error: Word must contain 5 characters")
+        exit()  # Ends the function early if a bad input is provided
+
+
+def input_letter() -> str:
+    letter: str = input("Enter a single character: ")
+    if len(letter) == 1:
+        return letter
+    else:
+        print("Error: Character must be a single character.")
+        exit()  # Ends the function early if a bad input is provided
+
+
+def contains_char(word: str, letter: str) -> None:
+    print("Searching for " + letter + " in " + word)
+    count = 0
+    if word[0] == letter:
+        print(letter + " found at index 0")
+        count = count + 1
+    if word[1] == letter:
+        print(letter + " found at index 1")
+        count = count + 1
+    if word[2] == letter:
+        print(letter + " found at index 2")
+        count = count + 1
+    if word[3] == letter:
+        print(letter + " found at index 3")
+        count = count + 1
+    if word[4] == letter:
+        print(letter + " found at index 4")
+        count = count + 1
+    # can be simplified with a for loop in future projects
+    # count = count + 1 can also use += for incriments
+    if count == 0:
+        print("No instances of " + letter + " found in " + word)
+    else:
+        print(str(count) + " instances of " + letter + " found in " + word)
+
+
+def main() -> None:
+    contains_char(word=input_word(), letter=input_letter())
+
+
+if __name__ == "__main__":
+    main()
